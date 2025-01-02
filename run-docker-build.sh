cd3e35023a2cc3a61b7cef26e9a3914912156cdb
Niklas Lakner <niklas.lakner@gmail.com>
2025-01-02 16:06:31 +0000

    fixes

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -5,4 +5,5 @@ docker build \
   --build-arg BUILD_FROM="homeassistant/$ARCH-base:latest" \
   --build-arg ARCH=$ARCH \
   -t local/my-test-addon \
-  .
+    .
+docker run --name my-test-addon --rm local/my-test-addon
