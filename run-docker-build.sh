392b6638cf51ce0ba807027a41934d7eab7120bd
Niklas Lakner <niklas.lakner@gmail.com>
2025-01-02 00:11:49 +0000

    repo fixes

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+ARCH=amd64
 cd s3-backup || exit
 docker build \
-  --build-arg BUILD_FROM="homeassistant/amd64-base:latest" \
+  --build-arg BUILD_FROM="homeassistant/$ARCH-base:latest" \
+  --build-arg ARCH=$ARCH \
   -t local/my-test-addon \
   .
